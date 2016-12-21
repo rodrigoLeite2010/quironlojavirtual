3c8bbccabf29a8633295242fa1d3b6e7fe0cf9b5
Rodrigodcp <rodrigo.soares.leite@gmail.com>
2016-12-21 15:10:34 -0200

    Initial

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+        public IEnumerable<Produto> Produto
+        {
+            get { return _context.Produto; }
+        }
+    }
+}
